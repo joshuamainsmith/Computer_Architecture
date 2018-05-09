d3b6adeb771559feba3e783989d0d7c0b779d026
joshuamainsmith <joshuamainsmith@gmail.com>
2018-05-08 18:02:21 -0700

    Added an option to quit

--- Computer Architecture/MIPS/Arithmetic/proj2.asm
+++ Computer Architecture/MIPS/Arithmetic/proj2.asm
@@ -1,11 +1,15 @@
 .data
 	prompt: .asciiz "\nEnter an integer: "
 	select: .asciiz "\nEnter 0 for add, 1 for subtract: "
+	quit: .asciiz "\n0 to quit, anything else to continue "
 	result: .asciiz "\nThe result is: "
 	
 .text
 .globl main
 main:
+	# Clearing $t0
+	addi $t0, $t0, 0
+	
 	#Display
 	li $v0, 4 #4 tells computer that string will be called
 	la $a0, prompt #displaying "prompt"
@@ -16,7 +20,7 @@ main:
 	syscall
 	
 	#Store result in $t0
-	move $t0, $v0 #addi?
+	move $t0, $v0
 	
 	#Display
 	li $v0, 4 #4 tells computer that string will be called
@@ -28,7 +32,7 @@ main:
 	syscall
 	
 	#Store result in $t1
-	move $t1, $v0 #addi?
+	move $t1, $v0 
 	
 	li $v0, 4 #4 tells computer that string will be called
 	la $a0, select #displaying "select"
@@ -41,7 +45,7 @@ main:
 	#Store result in $t2
 	move $t2, $v0 #addi?
 	
-	### do beq for whether $v0 == 0 (adding) or 1 (subtracting)
+	# If 0, add. Otherwise subtract
 	beq $t2, $zero, adding
 	beq $t2, 1, subtract
 	
@@ -77,8 +81,25 @@ subtract:
 	li $v0, 1 #printing int is 1
 	move $a0, $t0
 	syscall
+
+
+
+
+
 	
 end:
-j main
-li $v0,10
-syscall
+	li $v0, 4 #4 tells computer that string will be called
+	la $a0, quit #displaying "prompt"
+	syscall
+	
+	#Getting input
+	li $v0, 5 #Telling computer to input an integer
+	syscall
+	
+	#Store result in $t0
+	move $t0, $v0
+	
+	bne $t0, $zero, main
+	
+	li $v0,10
+	syscall
