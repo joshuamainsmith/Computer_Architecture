97097d6c772f0ace5cc2b9bfb4999afca225c6b1
joshuamainsmith <joshuamainsmith@gmail.com>
2018-05-12 15:03:37 -0700

    Added C example

--- Computer Architecture/MIPS/swap & strcpy/example.c
+++ Computer Architecture/MIPS/swap & strcpy/example.c
@@ -4,18 +4,65 @@
 
 int main ()
 {
-    int a = 5, b = 6;
+    int a = 5, b = 6, i = 0;
     char str1[7] = "pickle";
     char str2[7] = "banana";
 
+    printf ("Before a = %d, b = %d\n", a, b);
+
     swap (&a, &b);
 
-    printf ("a = %d, b = %d", a, b);
+    printf ("\nAfter a = %d, b = %d\n", a, b);
+
+    printf ("\n\nstr1 Before\n");
+
+    while (str1[i])
+    {
+        printf ("%c", str1[i]);
+
+        i++;
+    }
+
+    i = 0;
+
+    printf ("\n\nstr2 Before\n");
+
+    while (str2[i])
+    {
+        printf ("%c", str2[i]);
+
+        i++;
+    }
+
+    i = 0;
+
+
 
     scpy(&str1, &str2);
 
-    printf ("str1 = %c, str2 = %c", str1, str2);
 
+
+    printf ("\n\nstr1 After\n");
+
+    while (str1[i])
+    {
+        printf ("%c", str1[i]);
+
+        i++;
+    }
+
+    i = 0;
+
+    printf ("\n\nstr2 After\n");
+
+    while (str2[i])
+    {
+        printf ("%c", str2[i]);
+
+        i++;
+    }
+
+    printf ("\n");
 
     return 0;
 }
