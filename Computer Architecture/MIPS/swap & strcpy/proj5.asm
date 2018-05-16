ffe63fcdb45644c283ab78b06476d13c2c939801
joshuamainsmith <joshuamainsmith@gmail.com>
2018-05-16 10:06:59 -0700

    Deleted some subroutines

--- Computer Architecture/MIPS/swap & strcpy/proj5.asm
+++ Computer Architecture/MIPS/swap & strcpy/proj5.asm
@@ -84,6 +84,7 @@ main:
 	# Delete?
 	# jal strcpy
 	
+	add $sp, $sp, $s0
 	add $s0, $zero, $zero
 	
 	# Delete?
@@ -110,8 +111,12 @@ main:
 	
 	jal NewLine
 	
+	# Quitting program
+	li $v0,10
+	syscall
+	
 	# Jump to end of program
-	j end
+	# j end
 
 	
 #####################################
@@ -164,7 +169,6 @@ After:
 	
 	# Print integer "a" (s0)
 	li $v0, 1
-	#addi $a0, $s0, 0
 	lw $a0, ($s0)
 	syscall
 	
@@ -189,13 +193,16 @@ After:
 
 scpy:
 	# Store address from string2 into t0
-	add $t0, $s0, $a1
+	# add $t0, $s0, $a1
+	
+	lbu $t1, 0($a1)
+	addi $a1, $a1, 1
 	
 	# Load ch into t1
-	lbu $t1, 0($t0)
+	# lbu $t1, 0($t0)
 	
 	# Store ch onto the stack
-	sb $t1, 0($sp)
+	sb $t1, 0($sp)	
 	
 	# Store address from string1 into t2
 	add $t2, $s0, $a0
@@ -209,114 +216,13 @@ scpy:
 	# Increment through string2 address
 	addi $s0, $s0, 1
 	
-	# When finished, jump to return address
-	bne $t1, $zero, scpy
-	jr $ra
-
-
-
-
-
-
-
-
-# Delete subroutines?
-
-strcpy:
-	# Allocate space to the stack
-	addi $sp, $sp, -4
-	
-	# Store register s0 to the stack
-	sw $s0, 0($sp)
-
-	# Initialize s0 (counter) to zero
-	add $s0, $zero, $zero
-	
-	jr $ra
-
-strcpyLoop:
-	# string0 is in a1, string1 is in a2 (addresses)
-	
-	# Set a1 at s0 into t1
-	add $t1, $s0, $a1
-	
-	# Contents from t1 at index s0 to t2
-	lbu $t2, 0($t1)
-	
-	# Set a0 at index s0 into t3
-	add $t3, $s0, $a0
-	
-	# Store t3 into t2 (the string copy process)
-	sb $t2, 0($t3)
-	
-	# If finished, go to strcpyRestore
-	beq $t2, $zero, strcpyRestore
-	
-	# Else, increment s0
-	addi $s0, $s0, 1
-	
-	j strcpyLoop
-	
-strcpyRestore:
-	# Restore s0
-	lw $s0, 0($sp)
-	
-	# Deallocate the stack
-	addi $sp, $sp, 4
-	
-	jr $ra
-	
-test1:
-	add $t0, $s0, $a1
 	addi $sp, $sp, -1
 	
-	lbu $t1, 0($t0)
+	# When finished, jump to return address
+	bne $t1, $zero, scpy	
 	
-	sb $t1, 0($sp)
-	
-	addi $s0, $s0, 1
-	
-	bne $t1, $zero, test1
-	
-	addi $s1, $zero, 0
-	
-	j test2
-
-test2:
-	add $t2, $s1, $a0
-	
-	lbu $t5, 0($sp)
-	
-	sb $t5, 0($t6)
-	
-	addi $sp, $sp, 1
-	#
-	
-	lbu $t1, 0($fp)
-	
-	sb $t1, 0($t2)
-	
-	addi $s1, $s1, 1
-	
-	bne $t1, $zero, test2
-	
-	j restore
-
-restore:
-	#addi $sp, $sp, 1
-	
-	addi $fp, $fp, 1
-	
-	addi $s0, $s0, -1
-	
-	bne $s0, $zero, restore
-
 	jr $ra
-#
-	
-	
-	
-	
+
 NewLine:
 	# Make a new line
 	li $v0, 4
@@ -324,9 +230,3 @@ NewLine:
 	syscall
 	
 	jr $ra	
-
-	
-end:
-	# Quitting program
-	li $v0,10
-	syscall
